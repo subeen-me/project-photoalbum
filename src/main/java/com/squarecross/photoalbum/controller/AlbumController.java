427a397af1bf93c8ba7268e4044505e559e273c6
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-05-05 22:54:37 +0900

    AlbumController 추가, 앨범 상세 정보 조회 로직 추가

---
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -0,0 +1,35 @@
+package com.squarecross.photoalbum.controller;
+
+import com.squarecross.photoalbum.dto.AlbumDto;
+import com.squarecross.photoalbum.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    @Autowired
+    AlbumService albumService;
+
+    @RequestMapping(value = "{albumId}", method = RequestMethod.GET)
+    public ResponseEntity<AlbumDto> getAlbum(@PathVariable("albumId") final long albumId) {
+        AlbumDto album = albumService.getAlbum(albumId);
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam(value = "albumId") long albumId) {
+        AlbumDto album = albumService.getAlbum(albumId);
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/json_body", method = RequestMethod.POST)
+    public ResponseEntity<AlbumDto> getAlbumByJson(@RequestBody final AlbumDto albumDto) {
+        AlbumDto album = albumService.getAlbum(albumDto.getAlbumId());
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+}
